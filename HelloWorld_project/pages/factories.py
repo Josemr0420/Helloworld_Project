84ada8a64fb1ec8f1b2ad5ff20c9cbd9c6da27ac
_smithereens <82609853+Josemr0420@users.noreply.github.com>
2024-02-27 20:27:56 -0500

    Add files via upload

--- HelloWorld_project/pages/factories.py
+++ HelloWorld_project/pages/factories.py
@@ -1,8 +1,13 @@
-import factory
-from .models import Product  
+import factory 
 
-class ProductFactory(factory.django.DjangoModelFactory):     
-    class Meta:         
-        model = Product     
-        name = factory.Faker('company')     
-        price = factory.Faker('random_int', min=200, max=9000)
+from .models import Product 
+
+class ProductFactory(factory.django.DjangoModelFactory): 
+
+    class Meta: 
+
+        model = Product 
+
+    name = factory.Faker('company') 
+
+    price = factory.Faker('random_int', min=200, max=9000) 
--- HelloWorld_project/pages/urls.py
+++ HelloWorld_project/pages/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path 
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, contact_page
+from .views import HomePageView, AboutPageView, contactPageView, ProductIndexView, ProductCreateView, ProductShowView, ProductCreateView, ProductCreated, CartRemoveAllView, CartView,ImageViewFactory, ImageViewNoDI
+from .utils import ImageLocalStorage
 
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
+urlpatterns = [ 
+    path('', HomePageView.as_view(), name='home') ,
     path('about/', AboutPageView.as_view(), name='about'),
-    path('contact/', contact_page, name='contact'),
-    path('products/', ProductIndexView.as_view(), name='index'),
+    path('contact/', contactPageView, name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'), 
     path('products/create', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show')
-]
+    path('product_created/', ProductCreated, name='product-created'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'), 
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'), 
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'), 
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
+] 
